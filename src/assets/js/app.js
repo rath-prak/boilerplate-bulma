29d4eb42830bb06b55403b9bb28cd9440b20b2ac
Rath Prak <rathanap@gmail.com>
2019-08-06 17:06:12 +1200

    fix navigation, add page sections

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,16 @@
-console.log("gadfgsfhb");
+  //Active link to current page
+  let path = window.location.pathname.split('/').pop();
+  if (path == '') {
+    path = 'index.html';
+  }
+
+  let target = $('nav a[href="'+path+'"]');
+  let targetFooterMenu = $('.footer-menu a[href="'+path+'"]');
+  target.addClass('active');
+  targetFooterMenu.addClass('active');
+
+//  $(".nav a").on("click", function(){
+//     $(".nav").find(".active").removeClass("active");
+//     $(this).parent().addClass("active");
+//     console.log("testing");
+//   });
